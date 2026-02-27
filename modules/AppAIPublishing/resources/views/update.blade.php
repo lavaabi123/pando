70ddc6ce2ef952e2e7f1fd5c1a1f4be7f5f70bca
Lavanya <lavaabi@gmail.com>
2026-02-27 01:06:43 -0800

    design fix

--- modules/AppAIPublishing/resources/views/update.blade.php
+++ modules/AppAIPublishing/resources/views/update.blade.php
@@ -21,23 +21,23 @@
 <form class="actionForm" action="{{ module_url("save") }}" method="POST" data-redirect="{{ module_url(); }}">
     <div class="container max-w-800 pb-5">
 
-         <div class="mb-5">
+         <div class="mb-5 bg-white border rounded-5 p-4 inputField">
             @if($result)
                 <input class="form-control d-none" name="id" id="id" type="text" value="{{ old('name', $result->id_secure ?? '') }}">
             @endif
 
-            <div class="border border-gray-200 b-r-6 p-3 mb-3">
+            <div class="mb-3">
                 <label for="name" class="form-label">{{ __('Campaign name') }}</label>
                 <input 
                     placeholder="{{ __('Campaign name') }}" 
-                    class="form-control" 
+                    class="form-control border-2" 
                     name="name" 
                     id="name" 
                     type="text" 
                     value="{{ old('name', $result->name ?? '') }}">
             </div>
 
-            <div class="border border-gray-200 b-r-6 p-3 mb-3">
+            <div class="mb-3">
                @include('appchannels::block_channels', ['accounts' => $result->accounts ?? []])
             </div>
 
@@ -56,11 +56,11 @@
 
             @endforeach
 
-            <div class="border border-gray-200 b-r-6 mb-3">
+            <div class="border border-gray-300 b-r-15 mb-3">
                 @include('appaiprompts::block_prompts', ["prompts" => $prompts])
             </div>
 
-            <div class="border border-gray-200 b-r-6 mb-3">
+            <div class="border border-gray-300 b-r-15 mb-3">
                 <div class="d-flex border-bottom p-3 align-items-center justify-content-between">
                     <div class="fw-5 fs-14">{{ __("Options") }}</div>
                 </div>
@@ -74,7 +74,7 @@
                 </div>
             </div>
 
-            <div class="border border-gray-200 b-r-6 mb-3">
+            <div class="border border-gray-300 b-r-15 mb-3">
                 <div class="d-flex border-bottom p-3 align-items-center justify-content-between">
                     <div class="fw-5 fs-14">{{ __("Schedule Regularly") }}</div>
                 </div>
@@ -111,7 +111,7 @@
                     </div>
 
                     <div class="px-4 pb-4">
-                        <button type="button" class="btn btn-outline btn-dark w-100 addSpecificTimes">
+                        <button type="button" class="btn btn-dark w-100 addSpecificTimes">
                             <i class="fa-light fa-plus"></i> {{ __("Add time") }}
                         </button>
                     </div>
@@ -141,7 +141,7 @@
                     <div class="p-3 overflow-x-auto">
                         <div class="d-flex gap-8 min-w-400">
                             @foreach($weekdays as $value)
-                            <label for="{{ $value }}" class="flex-fill w-100 ratio-1x1 b-r-6 border border-gray-200 py-4 text-center bg-active-primary fw-3">
+                            <label for="{{ $value }}" class="flex-fill w-100 ratio-1x1 b-r-15 border-2 py-4 text-center bg-active-primary fw-6">
                                 {{ __($value) }}  
                                 <input class="form-check-input d-none" type="checkbox"  name="weekdays[{{ $value }}]" value="1" id="{{ $value }}" data-add-class="bg-primary text-white" {{ (isset($data['weekdays'][$value]) && $data['weekdays'][$value]) ? 'checked' : '' }}>
                             </label>
